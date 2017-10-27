91c029852073f194acbc8dc8978e9decb68e54dc
haoliang <haoliang0.1.2@gmail.com>
2017-10-27 19:30:41 +0800

    working-dir free

--- startup.sh
+++ startup.sh
@@ -5,11 +5,13 @@ docker rm workstation
 
 host_machine_ip=$(ip addr show | grep 'inet\b'  | awk '{ print $2 }' | grep -v '^172\|127' | sed 's/\/.*$//')
 
+workstation_path=$(dirname $(realpath $0))
+
 docker run -d \
-    -v $(pwd)/var/haoliang:/home/haoliang \
-    -v $(pwd)/var/root:/root \
+    -v $workstation_path/var/haoliang:/home/haoliang \
+    -v $workstation_path/var/root:/root \
     -v /srv/http:/srv/http \
-    -v $(pwd):/docker \
+    -v $workstation_path:/docker \
     -w /srv/http \
     -e HOST_MACHINE_IP=$host_machine_ip \
     -p "127.0.0.1:29000:9000" \
