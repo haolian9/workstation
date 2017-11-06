f40860174f8990190f3b03eb86199236de2bcdb1
haoliang <haoliang0.1.2@gmail.com>
2017-11-06 18:00:41 +0800

    mute error; set correct image name

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker stop workstation
-docker rm workstation
+workstation=sangwo/workstation
+
+docker stop workstation &>/dev/null
+docker rm workstation &>/dev/null
 
 # xdebug required
 host_machine_ip=$(ip addr show | grep 'inet\b'  | awk '{ print $2 }' | grep -v '^172\|127' | sed 's/\/.*$//')
@@ -18,5 +20,5 @@ docker run -d \
     -p "127.0.0.1:29000:9000" \
     --net=hub \
     --name workstation \
-    haoliangseiue/workstation
+    $workstation
 
