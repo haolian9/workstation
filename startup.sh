499184cc06385f132cafc35e74e61fbbf794d786
haoliang <haoliang0.1.2@gmail.com>
2018-03-19 16:38:56 +0800

    simplify naming

--- startup.sh
+++ startup.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-WS_IMG="sangwo/workstation:${WS_IMG:-latest}"
-WS_NAME=${WS_NAME:-workstation}
-WS_PATH=$(dirname $(realpath $0))
+IMG="sangwo/workstation:${tag:-'latest'}"
+NAME=${name:-"workstation"}
+
+ROOT=$(dirname $(realpath $0))
 # xdebug required
-HOST_MACHINE_IP=$(ip addr show | grep 'inet\b'  | awk '{ print $2 }' | grep -v '^172\|127' | sed 's/\/.*$//')
+HOST_IP=$(ip addr show | grep 'inet\b'  | awk '{ print $2 }' | grep -v '^172\|127' | sed 's/\/.*$//')
 
 {
-    docker stop $WS_NAME
-    docker rm $WS_NAME
+    docker stop $NAME
+    docker rm $NAME
 } &>/dev/null
 
 docker run -d \
-    -v $WS_PATH/var/haoliang:/home/haoliang \
-    -v $WS_PATH/var/root:/root \
+    -v $ROOT/var/haoliang:/home/haoliang \
+    -v $ROOT/var/root:/root \
     -v /srv/http:/srv/http \
     -v /srv/golang:/srv/golang \
     -w /srv/http \
-    -e XDEBUG_CONFIG="remote_host=${HOST_MACHINE_IP}" \
-    -e HOST_MACHINE_IP="${HOST_MACHINE_IP}" \
+    -e XDEBUG_CONFIG="remote_host=${HOST_IP}" \
+    -e HOST_IP="${HOST_IP}" \
     --net=hub \
-    --name $WS_NAME \
-    $WS_IMG
+    --name $NAME \
+    $IMG
 
