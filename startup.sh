bf9e32e8b423bcdf08b9fd47cc5b395c1b377aad
haoliang <haoliang0.1.2@gmail.com>
2018-05-30 01:28:00 +0800

    refactor startup.sh

--- startup.sh
+++ startup.sh
@@ -1,52 +1,13 @@
 #!/usr/bin/env bash
 
-determine_local_ip() {
-    local ip_list=()
-    local docker_spec='docker\|br-\|veth'
+ROOT=$(dirname $(realpath $0))
 
-    for dev in $(ip link | grep '^[0-9]\+:' | awk '{ print $2 }' | grep -v lo | grep -v $docker_spec | tr -d : ); do
-        local my_ip=""
-        my_ip=$(ip addr show dev $dev | grep 'inet\b' | awk '{ print $2 }' | sed 's/[\/].*$//')
-        if [ -z "$my_ip" ]; then
-            continue;
-        fi
-        ip_list+=("$my_ip")
-    done
-
-    if [ "${#ip_list[@]}" -gt 1 ]; then
-        >&2 echo "can not determine the ip, found: ${ip_list[*]}"
-        return 1
-    fi
-
-    echo ${ip_list[0]}
-}
-
-available_memory() {
-    local percent=${1:?requires percent arg}
-
-    local total=$(lsmem -b -o SIZE | grep -i 'total online memory:' | grep '[0-9]\+' --only-matching)
-
-    [ -z "$total" ] && {
-        >&2 echo "invalid total memory, '$total'"
-        return 1
-    }
-
-    printf "%.2fG" $(echo "$total / 1024/1024/1024 * $percent" | bc -l)
-}
-
-available_cpu() {
-    local percent=${1:?requires percent arg}
-    local total=$(nproc)
-
-    printf "%.1f" $(echo "$total * $percent" | bc -l)
-}
+source $ROOT/scripts/util.sh
 
 #############################################################################
 # configuration
 
-ROOT=$(dirname $(realpath $0))
-
-IMG="sangwo/workstation:${tag:-latest}"
+IMG="${image:-sangwo/workstation:latest}"
 NAME=${name:-workstation}
 # todo check port is usable
 PUBLISH_PORT="${publish_port:-127.0.0.0:8000-8002:8000-8002}"
@@ -58,31 +19,44 @@ CPU_LIMIT=$(available_cpu ${cpu_percent:-0.8})
 #############################################################################
 # main
 
-{
-    docker stop $NAME
-    docker rm $NAME
-} &>/dev/null
+clean() {
+    {
+        docker stop $NAME
+        docker rm $NAME
+    } &>/dev/null
 
-echo "using image $IMG, and running container be named as $NAME"
+    return 0
+}
 
-docker run -d \
-    -v $ROOT/var/haoliang:/home/haoliang \
-    -v $ROOT/var/root:/root \
-    -v /srv/http:/srv/http \
-    -v /srv/golang:/srv/golang \
-    -w /srv/http \
-    -e XDEBUG_CONFIG="remote_host=${HOST_IP}" \
-    -e HOST_IP="${HOST_IP}" \
-    -p "${PUBLISH_PORT}" \
-    -m "${MEMORY_LIMIT}" \
-    --cpus "${CPU_LIMIT}" \
-    --net=hub \
-    --name $NAME \
-    $IMG
+run() {
+    docker run -d \
+        -v $ROOT/var/haoliang:/home/haoliang \
+        -v $ROOT/var/root:/root \
+        -v /srv/http:/srv/http \
+        -v /srv/golang:/srv/golang \
+        -w /srv/http \
+        -e XDEBUG_CONFIG="remote_host=${HOST_IP}" \
+        -e HOST_IP="${HOST_IP}" \
+        -p "${PUBLISH_PORT}" \
+        -m "${MEMORY_LIMIT}" \
+        --cpus "${CPU_LIMIT}" \
+        --net=hub \
+        --name $NAME \
+        $IMG
+}
 
-if [ $? -eq 0 ]; then
+main() {
+
+    clean
+
+    echo "using image $IMG, and running container be named as $NAME"
+
+    if ! run; then
+        echo "just can not start the $NAME"
+        return 1
+    fi
+
     echo "everything is fine."
-else
-    echo "just can not start the $NAME"
-    exit 1
-fi
+}
+
+cd $ROOT && main
