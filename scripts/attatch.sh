c9e77377182ac52f1d5230c33f15883507ec2380
haoliang <haoliang0.1.2@gmail.com>
2018-09-10 09:53:34 +0800

    removed workaround

--- scripts/attatch.sh
+++ scripts/attatch.sh
@@ -7,8 +7,4 @@ if [ $( docker ps --filter="name=$CONTAINER" | wc -l ) -lt 2 ]; then
     exit 1
 fi
 
-# see https://github.com/moby/moby/issues/35407
-# but did not work
-#termArgs="$(printf -- '-e COLUMNS=%d -e LINES=%d' "$(tput cols)" "$(tput lines)")"
-
 exec docker exec -it $CONTAINER zsh
