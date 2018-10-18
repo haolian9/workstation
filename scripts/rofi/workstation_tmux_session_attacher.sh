2e26970235b1aa65e4fca8c30c176b64ef465afb
haoliang <haoliang0.1.2@gmail.com>
2018-10-18 19:04:11 +0800

    [rofi.plugin] default session

--- scripts/rofi/workstation_tmux_session_attacher.sh
+++ scripts/rofi/workstation_tmux_session_attacher.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 show_options() {
+    create_default_session_if_necessary &>/dev/null
+    list_sessions
+}
+
+list_sessions() {
     INTERACTIVE_TTY="" $UTIL_DIR/attach.sh tmux list-session -F '#{session_name}' 2>&1
+}
+
+create_default_session_if_necessary() {
+    list_sessions | grep '^default$' &>/dev/null || {
+        INTERACTIVE_TTY="" $UTIL_DIR/attach.sh tmux new-session -d -c "$HOME" -s default
+    }
 }
 
 active_option() {
